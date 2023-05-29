183578ac02a7c9b679e2935a3316e5fe4418cd66
itzzzyashu@gmail.com <sakshamvirmani86@gmail.com>
2023-05-29 20:40:44 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m RajniiRobot
+ps:scale worker=1
---
+++ RajniiRobot/modules/tagger.py
@@ -0,0 +1,5 @@
+__mod_name__ = "Mention all"
+
+__help__ = """
+◇ `/tagall` or `/mentionall` or `#all` or `@all` : Tag everyone in a chat
+"""
